301b46dee399e30f86ed81625c758c9f6eb7c9ea
karadagg0 <104911016+karadagg0@users.noreply.github.com>
2022-12-11 16:58:17 +0300

    system for changing direction in x-axis while character is running
    system for changing direction in x-axis while character is running

---
+++ Change/Change/character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * .5f * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - .1f, transform.position.y, transform.position.z), .3f);
+            }
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + .1f, transform.position.y, transform.position.z), .3f);
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
